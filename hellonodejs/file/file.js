3898869caa10d9a7fd603df36348d8bab3f80936
weijingwei <weijingwei@sina.com>
2016-01-27 14:45:27 +0800

    buffer reader

--- hellonodejs/file/file.js
+++ hellonodejs/file/file.js
@@ -2,13 +2,49 @@
  * http://usejsdoc.org/
  */
 var fs = require("fs");
-fs.readFile("../README.md", function(err, data) {
+fs.readFile("input.txt", function(err, data) {
 	if (err) {
 		return console.error(err);
 	}
 	console.log("异步读取：" + data.toString());
 });
 
-var data = fs.readFileSync("../README.md");
+var data = fs.readFileSync("input.txt");
 console.log("同步读取：" + data);
 console.log("程序执行完毕。");
+
+fs.open("input.txt", "r+", function(err, fd) {
+	if (err) {
+		return console.error(err);
+	}
+	console.dir(fd);
+});
+
+console.log("准备打开文件！");
+fs.stat('input.txt', function (err, stats) {
+   if (err) {
+       return console.error(err);
+   }
+   console.log(stats);
+   console.log("读取文件信息成功！");
+   
+   // 检测文件类型
+   console.log("是否为文件(isFile) ? " + stats.isFile());
+   console.log("是否为目录(isDirectory) ? " + stats.isDirectory());    
+});
+
+console.log("准备写入文件");
+fs.writeFile('output.txt', '我是通过写入的文件内容！',  function(err) {
+   if (err) {
+       return console.error(err);
+   }
+   console.log("数据写入成功！");
+   console.log("--------我是分割线-------------");
+   console.log("读取写入的数据！");
+   fs.readFile('output.txt', function (err, data) {
+      if (err) {
+         return console.error(err);
+      }
+      console.log("异步读取写入文件数据: " + data.toString());
+   });
+});
